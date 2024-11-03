8daa7f890ae268c85368262f6aec62952474929f
marcelbinz <marcelbinz1@gmail.com>
2024-11-03 22:21:19 +0100

    update generalization

--- generalization/generalization_custom_metrics.py
+++ generalization/generalization_custom_metrics.py
@@ -33,60 +33,59 @@ def compute_metrics(pred):
 	return {'custom_loss': pred.predictions.mean()}
 
 if __name__ == '__main__':
-      parser = argparse.ArgumentParser()
-      parser.add_argument("--model", type=str, required=True)
-      args = parser.parse_args()
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--model", type=str, required=True)
+	args = parser.parse_args()
 
-      task_names = [
-            "jansen2021logic/prompts.jsonl",
-      ]
+	task_names = [
+    	"jansen2021logic/prompts.jsonl",
+	]
 
-      model, tokenizer = FastLanguageModel.from_pretrained(
-        model_name = args.model,
-        max_seq_length = 32768,
-        dtype = None,
-        load_in_4bit = True,
-      )
-      l_id = tokenizer(" <<").input_ids[1:]
-      r_id = tokenizer(">>").input_ids[1:]
-      collator = DataCollatorForCompletionOnlyLM(response_template=l_id, instruction_template=r_id, tokenizer=tokenizer)
-      is_quantized = model.is_quantized
+	model, tokenizer = FastLanguageModel.from_pretrained(
+    	model_name = args.model,
+    	max_seq_length = 32768,
+    	dtype = None,
+    	load_in_4bit = True,
+	)
+	l_id = tokenizer(" <<").input_ids[1:]
+	r_id = tokenizer(">>").input_ids[1:]
+	collator = DataCollatorForCompletionOnlyLM(response_template=l_id, instruction_template=r_id, tokenizer=tokenizer)
+	is_quantized = model.is_quantized
 
-	  data = []
-	  with torch.no_grad():
-            for i, task_name in enumerate(task_names):
-                  dataset = load_dataset(
-                        'json',
-                        data_files={
-                              'test': [task_name],
-                        }
-                  )
+	data = []
+	with torch.no_grad():
+		for i, task_name in enumerate(task_names):
+			dataset = load_dataset(
+				'json',
+				data_files={
+					'test': [task_name],
+				}
+			)
 
-                  model.is_quantized = False
-                  training_args = TrainingArguments(
-                        output_dir="eval_"+str(i),
-                        per_device_eval_batch_size=1,
+			model.is_quantized = False
+			training_args = TrainingArguments(
+				output_dir="eval_"+str(i),
+				per_device_eval_batch_size=1,
 				eval_accumulation_steps=1,
-                        report_to="none"
-                  )
-                  trainer = SFTTrainer(
-                        model=model,
-                        tokenizer=tokenizer,
-                        args=training_args,
-                        train_dataset=dataset['test'],
-                        eval_dataset=dataset['test'],
-                        dataset_text_field="text",
-                        max_seq_length=32768,
-                        data_collator=collator,
+				report_to="none"
+			)
+			trainer = SFTTrainer(
+    			model=model,
+				tokenizer=tokenizer,
+				args=training_args,
+				train_dataset=dataset['test'],
+				eval_dataset=dataset['test'],
+				dataset_text_field="text",
+				max_seq_length=32768,
+				data_collator=collator,
 				compute_metrics=compute_metrics,
 				preprocess_logits_for_metrics=preprocess_logits_for_metrics,
-                  )
-                  model.is_quantized = is_quantized
-                  result = trainer.evaluate()
-
-                  print(task_name, flush=True)
-                  print(result, flush=True)
-                  data.append([task_name, result['eval_custom_loss']])
-            df = pd.DataFrame(data, columns=['task', str(args.model)])
-            print(df, flush=True)
-            df.to_csv('results/custom_metrics_' + args.model.replace('/', '-') +  '.csv')
+			)
+			model.is_quantized = is_quantized
+			result = trainer.evaluate()
+			print(task_name, flush=True)
+			print(result, flush=True)
+			data.append([task_name, result['eval_custom_loss']])
+		df = pd.DataFrame(data, columns=['task', str(args.model)])
+		print(df, flush=True)
+		df.to_csv('results/custom_metrics_' + args.model.replace('/', '-') +  '.csv')
---
+++ scripts/cluster_eval_generalization_custom_metrics.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH -J CENTaUR2
+#SBATCH -p gpu_p
+#SBATCH --qos gpu_normal
+#SBATCH --gres=gpu:1
+#SBATCH --mem=80G
+#SBATCH -t 24:00:00
+#SBATCH --constraint=a100_80gb
+#SBATCH --nice=10000
+#SBATCH --cpus-per-task=20
+
+cd ../generalization/
+
+source activate unsloth_env2
+
+python generalization_custom_metrics.py --model marcelbinz/Llama-3.1-Centaur-70B-adapter
